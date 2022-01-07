bbf5706b042df964999a9ec59896754773b44243
DameK <hwanseo6417@naver.com>
2022-01-07 11:43:25 +0900

    [BOJ/C++]1789-수들의 합

--- C++/Silver/1411.cpp
+++ C++/Silver/1411.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 
 int main(){
+    /*
+    f: 하나하나씩 비교하는 중간에 틀린 여부 확인해주는 변수 
+    visit1, visit2 = 단어 한글자씩 비교하기 위해 필요한 변수
+    */
     bool f = 0;
     int t, visit1[26], visit2[26], ans = 0;
     string str[101];
@@ -24,6 +28,7 @@ int main(){
             memset(visit2, -1, sizeof(visit2));
 
             for (int k = 0, len = str[i].length(); k < len; ++k) {
+                // 한 글자씩 비교 
                 if (visit1[str[i][k] - 'a'] != -1 && visit1[str[i][k] - 'a'] != str[j][k] - 'a') {
                     f = 1;
                     break;
@@ -34,7 +39,6 @@ int main(){
                 }
                 visit1[str[i][k] - 'a'] = str[j][k] - 'a';
                 visit2[str[j][k] - 'a'] = str[i][k] - 'a';
-                cout << str[i][k]<<' '<<visit2[str[j][k]]<<'\n';
             }
             if (f) continue;
             ans++;
