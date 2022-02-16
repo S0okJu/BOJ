81d11ac55b701062b0dda4dfb349a14f3d5690ec
DameK <hwanseo6417@naver.com>
2022-02-16 11:16:28 +0900

    [BOJ/Python]1010-다리 놓기

---
+++ Python/Silver/1010.py
@@ -0,0 +1,22 @@
+Tcase =  int(input())
+dp = [[0]* 31 for _ in range(31)]
+
+for i in range(1,31):
+    dp[0][i] = 1
+    dp[i][i] = 1
+
+for i in range(1,30):
+    for j in range(1,(30-i)+1):
+        dp[j][i+j] = dp[j-1][i+j-1]+dp[j][i+j-1]
+
+for t in range(Tcase):
+    N, M = map(int, input().split())
+    print(dp[N][M])
+    
+    # print('\n')
+
+                
+
+
+
+
