9aa8cf8f429b0efd756472dfdc4e87493736d37f
DameK <hwanseo6417@naver.com>
2022-02-23 10:15:08 +0900

    [BOJ/C++]9081-단어 맞추기

---
+++ C++/Silver/9081.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+
+
+int main(){
+    int T;
+    cin >> T;
+
+    while(T--){
+        string a, ans;
+        int cnt = 0;
+
+        cin >> a;
+        do{
+            ans = a;
+            if(++cnt == 2)break;
+        }while(next_permutation(a.begin(),a.end()));
+        cout << ans<<'\n';
+
+    }
+}
