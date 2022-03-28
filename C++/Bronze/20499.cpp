46681707b30ae8274e4a9c97714bb778f83f0a38
DameK <hwanseo6417@naver.com>
2022-03-28 09:32:34 +0900

    Darius님 한타 안함?

---
+++ C++/Bronze/20499.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    string s;
+    int arr[3];
+    int idx= 0;
+    cin >> s;
+    
+    idx = s.find('/');
+    arr[0]=stoi(s.substr(0,idx));
+
+    int before = idx;
+    idx = s.find('/',before+1);
+    arr[1] = stoi(s.substr(before+1,idx - before-1));
+
+    arr[2]=stoi(s.substr(idx+1));
+    
+    if(arr[0]+arr[2]<arr[1]||arr[1]==0){
+        cout << "hasu";
+    }
+    else{
+        cout <<"gosu";
+    }
+    return 0;
+}
