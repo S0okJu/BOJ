c8dada46f413e1ba136a7ba416c8c947554fcff5
DameK <hwanseo6417@naver.com>
2022-03-31 09:19:34 +0900

    [BOJ/C++]24883-자동완성

---
+++ C++/Bronze/24883.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string a;
+    cin >> a;
+
+    if(a=="N"||a=="n"){
+        cout << "Naver D2";
+    }
+    else{
+        cout <<"Naver Whale";
+    }
+    return 0;
+}
