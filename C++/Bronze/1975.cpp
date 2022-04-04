51004fff154c72cacd758a7bc245597de4e3f10a
DameK <hwanseo6417@naver.com>
2022-04-04 09:05:31 +0900

    [BOJ/C++]1975-Number Game

---
+++ C++/Bronze/1975.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int t, n;
+
+int f(int n, int i){
+    int cnt = 0;
+    while(n){
+        if(n%i==0)cnt++;
+        else break;
+
+        n/=i;
+    }
+    return cnt;
+}
+
+int main(){
+    fastio;
+    cin >> t;
+    
+    for(int i = 0 ; i < t;i++){
+        cin >> n;
+        int ans = 0;
+
+        for(int i =2; i <=1000;i++){
+            ans +=f(n,i);
+        }
+        cout << ans <<"\n";
+    }
+
+    return 0;
+}
