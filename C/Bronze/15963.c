a3cfe023886ea5529e71fcf280afd021363e3c96
DameK <hwanseo6417@naver.com>
2022-04-15 10:14:41 +0900

    [BOJ/C]15963-CASIO

---
+++ C/Bronze/15963.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void){
+    int N, M;
+    scanf("%d %d",&N, &M);
+
+    if(M==N){
+        printf("1");
+    }
+    else{
+        printf("0");
+    }
+    return 0;
+}
