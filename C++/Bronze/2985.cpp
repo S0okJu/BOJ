cb2d272ac38ec9c19aaf9d8ca2edbafb7edb5ead
DameK <hwanseo6417@naver.com>
2022-05-14 11:05:22 +0900

    [BOJ/C++]2985-세 수

---
+++ C++/Bronze/2985.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double a,b,c;
+    cin >> a >> b >> c;
+    if(a + b == c)
+        cout << a << '+' << b << '=' <<c;
+    else if(a - b == c)
+        cout << a << '-' << b << '=' <<c;
+    else if(a * b == c)
+        cout << a << '*' << b << '=' <<c;
+    else if(a / b == c)
+        cout << a << '/' << b << '=' <<c;
+    else if(a == b + c)
+        cout << a << '=' << b << '+' <<c;
+    else if(a == b - c)
+        cout << a << '=' << b << '-' <<c;
+    else if(a == b * c)
+        cout << a << '=' << b << '*' <<c;
+    else if(a == b / c)
+        cout << a << '=' << b << '/' <<c;
+}
