9f0d6c0e67d1cba1534b454e8b1d049c576b243b
D7MeKz <damekyung98@gmail.com>
2024-05-27 17:52:24 +0900

    [BOJ/Go] 1356-유진수

---
+++ Go/Bronze/1356.go
@@ -0,0 +1,36 @@
+package Bronze
+
+import "fmt"
+
+func ReputationCalculator(inp string) int64 {
+	var result int64 = 1
+	for i := 0; i < len(inp); i++ {
+		result *= int64(inp[i] - '0')
+	}
+	return result
+}
+
+func Bronze1356() {
+	// 문자열로 입력받는다.
+	var input string
+	fmt.Scanln(&input)
+
+	// 일의 자리인 경우에는 무조건 0이다.
+	if len(input) == 1 {
+		fmt.Print("NO")
+		return
+	}
+
+	// 앞부분, 뒷부분에 해당되는 값을 구한다.
+	var front, back int64
+	flag := "NO"
+	for i := 0; i < len(input); i++ {
+		front = ReputationCalculator(input[:i])
+		back = ReputationCalculator(input[i:])
+		if front == back {
+			flag = "YES"
+			break
+		}
+	}
+	fmt.Print(flag)
+}
