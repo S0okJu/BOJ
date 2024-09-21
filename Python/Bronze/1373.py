0cac9306d91bbd1ad724dfe73570e364643c3317
S0okJu <damekyung98@gmail.com>
2024-09-22 00:36:36 +0900

    [BOJ/Python] 1373

--- Python/Bronze/1373.py
+++ Python/Bronze/1373.py
@@ -1,10 +1,34 @@
 def solution(bits):
     num_list = list()
+    bits_len = len(bits)
+    
+    rot_cnt = bits_len // 3
+    for i in range(rot_cnt):
+        rot_idx = bits_len - (int(i) * 3) 
+        num_list.append(bits[rot_idx -3:rot_idx])
         
+    remain = bits_len % 3 
+    if remain != 0:
+        zero = ""
+        for j in range(3 - remain):
+            zero += "0"
+        
+        num_list.append(zero + bits[:remain])
+        
+    result = converter(num_list)
+    
     return result
 
+def converter(num_list):
+    num_sum_list = list()
+    for num in num_list:
+        num_sum = 0
+        num_sum += ( (2 ** 2) * int(num[0])) + ( (2 ** 1) * int(num[1])) + ( (2 ** 0) * int(num[2]))
+        num_sum_list.append(num_sum)
+    
+    return num_sum_list
 
 if __name__ == "__main__":
     bits = input()
     result = solution(bits)
-    print(result)
+    print(''.join(str(x) for x in reversed(result)))
