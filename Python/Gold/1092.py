7fac15764e92e7654ccc56d3cf4c1e4754da7144
S0okJu <damekyung98@gmail.com>
2024-10-26 11:05:59 +0900

    [BOJ/Python] 1092

--- Python/Gold/1092.py
+++ Python/Gold/1092.py
@@ -2,24 +2,32 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input())
-cranes = sorted(list(map(int, input().split())), reverse=True)
+def solution(cranes, boxes):
+    time = 0
+    # 1. 불가능한 경우 
+    if cranes[0] < boxes[0]:
+        time = -1
+    else:
+        while boxes:
+            for crane in cranes:
+                # 다음 크레인으로 넘기는 경우 
+                if boxes and crane < boxes[-1]:
+                    continue
+                # 담는 경우 
+                for box in boxes:
+                    if crane >= box:
+                        boxes.remove(box)
+                        break
+            
+            time +=1
+    
+    print(time)
+        
+if __name__ == '__main__':
+    n = int(input())
+    cranes = sorted(list(map(int, input().split())), reverse=True)
 
-m = int(input())
-boxes = sorted(list(map(int, input().split())),reverse=True)
+    m = int(input())
+    boxes = sorted(list(map(int, input().split())),reverse=True)
 
-time = 0
-if boxes[0] > cranes[0]:
-    time = -1
-else:
-    while boxes:
-        for c in cranes:
-            if boxes and c < boxes[-1]:
-                continue
-            else:
-                for b in boxes:
-                    if b <= c:
-                        boxes.remove(b)
-                        break 
-        time += 1
-print(time)
+    solution(cranes, boxes)
