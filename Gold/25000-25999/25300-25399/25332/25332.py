189d3750a3e5065aa79213f2931d7b4142c48faf
S0okJu <damekyung98@gmail.com>
2025-03-18 11:36:42 +0900

    [BOJ/Go] 25332

---
+++ Gold/25000-25999/25300-25399/25332/25332.py
@@ -0,0 +1,26 @@
+import sys
+from collections import defaultdict
+
+input = sys.stdin.readline
+
+N = int(input().strip())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+count_map = defaultdict(int)
+count_map[0] = 1  
+
+count = 0
+diff_sum = 0  
+
+for i in range(N):
+    # D[i] = (A의 누적합) - (B의 누적합)
+    diff_sum += A[i] - B[i]  
+    
+    # 누적합이 같으면 무조건 diff_sum은 0이 나옴 
+    count += count_map[diff_sum]
+
+    count_map[diff_sum] += 1
+
+
+print(count)
