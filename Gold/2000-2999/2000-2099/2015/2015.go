8aef38f9d3f3f447e07ffe058a78d6116163c93b
S0okJu <damekyung98@gmail.com>
2025-03-18 11:01:41 +0900

    [BOJ/Go] 2015

--- Gold/2000-2999/2000-2099/2015/2015.go
+++ Gold/2000-2999/2000-2099/2015/2015.go
@@ -1,1 +1,46 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	firstLine, _ := reader.ReadString('\n')
+	parts := strings.Fields(firstLine)
+	n, _ := strconv.Atoi(parts[0])
+	k, _ := strconv.Atoi(parts[1])
+
+	secondLine, _ := reader.ReadString('\n')
+	numStrings := strings.Fields(secondLine)
+	inputData := make([]int, n)
+	for i, numStr := range numStrings {
+		inputData[i], _ = strconv.Atoi(numStr)
+	}
+
+	// 0으로 초기화
+	sumDict := make(map[int]int)
+	sumDict[0] = 1
+
+	sum := 0
+	answer := 0
+
+	for _, data := range inputData {
+		// fmt.Println(data)
+		sum += data
+
+		if val, exists := sumDict[sum-k]; exists {
+			answer += val
+		}
+
+		sumDict[sum]++
+	}
+
+	fmt.Println(answer)
+}
