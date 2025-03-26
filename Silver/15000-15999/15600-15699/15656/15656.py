fe6ef530c596dd1450c056465d6d59e714ec3c94
S0okJu <damekyung98@gmail.com>
2025-03-26 09:37:56 +0900

    [BOJ/Python] 15656

---
+++ Silver/15000-15999/15600-15699/15656/15656.py
@@ -0,0 +1,28 @@
+import sys 
+input = sys.stdin.readline
+
+def solution(N, M, lst):
+    result = []
+    graph = []
+    
+    lst.sort()
+    
+    def backtracking(graph):
+        if len(graph) == M:
+            result.append(graph[:])
+            return 
+        
+        for i in range(N):
+
+            graph.append(lst[i])
+            backtracking(graph)
+            graph.pop()
+
+    backtracking(graph)
+    return result 
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+    n_lst = list(map(int,input().split()))
+    
+    for r in solution(N,M,n_lst):
+        print(*r)
