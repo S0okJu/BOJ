3625bda9ade68d9ebdb05b917d38a0db251e841b
S0okJu <damekyung98@gmail.com>
2025-03-30 15:32:04 +0900

    [BOJ/Python] 1946

---
+++ Silver/1000-1999/1900-1999/1946/1946.py
@@ -0,0 +1,34 @@
+import sys 
+input =  sys.stdin.readline
+
+def solution(N, cands):
+    
+    # [(1, 4), (2, 5), (3, 6), (4, 2), (5, 7), (6, 1), (7, 3)]
+    cands.sort()
+    
+    # 1차 1등은 우선 선별함 
+    result = 1
+    
+    # 2차를 기준으로 합격자 선발 
+    # 1차에서 1등한 합격자의 2차 순위를 기반으로 합격자 선별 
+    
+    min_second = cands[0][1]
+    
+    for i in range(1,N):
+        if cands[i][1] < min_second:
+            result +=1
+            min_second = cands[i][1]
+    
+    return result
+            
+    
+if __name__ == '__main__':
+    T = int(input())
+    for i in range(T):
+        N = int(input())
+        cands = []
+        for i in range(N):
+            first, second = map(int,input().split())
+            cands.append((first,second))
+        
+        print(solution(N, cands))
