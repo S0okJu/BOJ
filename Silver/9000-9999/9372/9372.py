1264012ffd57c913f3f344a24125fac351d17b47
S0okJu <damekyung98@gmail.com>
2025-04-02 11:15:59 +0900

    [BOJ/Python] 9372

---
+++ Silver/9000-9999/9372/9372.py
@@ -0,0 +1,14 @@
+import sys 
+input = sys.stdin.readline
+
+if __name__ == '__main__':
+    T = int(input())
+    for i in range(T):
+        
+        N, M = map(int,input().split())
+        
+        for j in range(M):
+            a,b = map(int,input().split())
+
+        print(N-1)
+        
