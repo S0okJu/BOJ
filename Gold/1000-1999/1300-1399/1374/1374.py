9f4a25a843c80f58ad97a73077635f05bfdf2db2
S0okJu <damekyung98@gmail.com>
2025-04-09 17:57:16 +0900

    [BOJ/Python] 1374

---
+++ Gold/1000-1999/1300-1399/1374/1374.py
@@ -0,0 +1,23 @@
+from typing import List 
+from heapq import heappush,heappop
+import sys 
+input = sys.stdin.readline
+
+def solution(N:int, lessons:List[List[int]]) -> int:
+    # 시작 시간을 기준으로 정렬 
+    lessons = sorted(lessons, key=lambda x:x[1])
+    classrooms = []
+    
+    for lesson_id, start, end in lessons:
+        if classrooms and classrooms[0][0] <=start:
+            heappop(classrooms)
+        heappush(classrooms, (end,lesson_id))
+    print(classrooms)
+    return len(classrooms)
+        
+            
+        
+if __name__ == '__main__':
+    N = int(input())
+    lessons = [list(map(int,input().split())) for _ in range(N)]
+    print(solution(N,lessons))
