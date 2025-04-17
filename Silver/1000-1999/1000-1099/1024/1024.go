8a8e77a2e62da4b3c2539ef5515b2a1fdfab10c8
S0okJu <damekyung98@gmail.com>
2025-04-17 19:26:41 +0900

    [BOJ/Go] 1024

---
+++ Silver/1000-1999/1000-1099/1024/1024.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func Solution(N int, L int) []int {
+	for length := L; length <= 100; length++ {
+		temp := N - (length*(length-1))/2
+
+		if temp < 0 {
+			continue
+		}
+
+		if temp%length == 0 {
+			a := temp / length
+			result := make([]int, length)
+			for i := 0; i < length; i++ {
+				result[i] = i + a
+			}
+			return result
+		}
+
+	}
+
+	return nil
+}
+func main() {
+	var N, L int
+	fmt.Scan(&N, &L)
+
+	result := Solution(N, L)
+	if result == nil {
+		fmt.Println(-1)
+
+	} else {
+		for _, v := range result {
+			fmt.Printf("%d ", v)
+		}
+	}
+}
