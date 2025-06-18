4b65ef6b5b3498d1fd68ca18f35d7150b91da6a5
S0okJu <damekyung98@gmail.com>
2025-06-18 11:01:31 +0900

    [BOJ/Go] 1181

--- 0-9999/1000-1999/1100-1199/1181/go/1181.go
+++ 0-9999/1000-1999/1100-1199/1181/go/1181.go
@@ -9,6 +9,26 @@ import (
 	"strings"
 )
 
+func sortByCondition(words []string) {
+	sort.Slice(words, func(i, j int) bool {
+		if len(words[i]) == len(words[j]) {
+			return words[i] < words[j]
+		}
+		return len(words[i]) < len(words[j])
+	})
+}
+
+func removeDuplication(words []string) []string {
+	var result []string
+
+	for i, word := range words {
+		if i == 0 || word != words[i-1] {
+			result = append(result, word)
+		}
+	}
+	return result
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
@@ -23,21 +43,8 @@ func main() {
 		words[i] = line
 	}
 
-	// Sorting
-	sort.Slice(words, func(i, j int) bool {
-		if len(words[i]) == len(words[j]) {
-			return words[i] < words[j] // 사전순
-		}
-		return len(words[i]) < len(words[j]) // 길이 오름차순
-	})
-
-	// Remove duplication
-	var result []string
-	for i, word := range words {
-		if i == 0 || word != words[i-1] {
-			result = append(result, word)
-		}
-	}
+	sortByCondition(words)
+	result := removeDuplication(words)
 
 	for _, word := range result {
 		fmt.Println(word)
