0ca1df398a45e012cda7cd0ffb4cd79a78e4d09c
S0okJu <damekyung98@gmail.com>
2025-06-20 21:07:38 +0900

    [BOJ/Python] 9498

---
+++ 0-9999/9000-9999/9400-9499/9498/9498.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    score = int(input())
+    
+    result = ''
+    if 90 <= score <= 100:
+        result = 'A'
+    elif 80 <= score < 90:
+        result = 'B'
+    elif 70 <= score < 80:
+        result = 'C'
+    elif 60 <= score < 70:
+        result = 'D'
+    else:
+        result = 'F'
+    
+    print(result)
+    
