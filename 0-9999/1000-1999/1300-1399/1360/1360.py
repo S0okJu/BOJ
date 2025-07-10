5599c654e83fceea6f68f87a8a6a1541d6e9b6c7
S0okJu <damekyung98@gmail.com>
2025-07-10 17:26:05 +0900

    [BOJ/Python] 1360

--- 0-9999/1000-1999/1300-1399/1360/1360.py
+++ 0-9999/1000-1999/1300-1399/1360/1360.py
@@ -1,16 +1,13 @@
 def solve():
     N = int(input())
     
-    # 명령어 저장: (시간, 명령어타입, 내용)
     commands = []
     for _ in range(N):
         cmd, tgt, time  = input().strip().split()
         commands.append((int(time), cmd, tgt))
     
-    # 역순으로 각 명령어의 실행 여부를 계산
     executed = [True] * len(commands)
     
-    # 뒤에서부터 앞으로 처리
     for i in range(len(commands) - 1, -1, -1):
         time, cmd_type, param = commands[i]
         
@@ -23,7 +20,6 @@ def solve():
                 if time - int(param) <= target_time < time:
                     executed[j] = not executed[j]
     
-    # 최종 결과 구성
     result = []
     for i in range(len(commands)):
         if commands[i][1] == 'type' and executed[i]:
